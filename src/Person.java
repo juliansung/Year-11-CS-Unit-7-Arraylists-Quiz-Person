576c62c749333c5950ac50e98a7fe26288467022
juliansung <julians2025@student.cis.edu.hk>
2023-03-07 12:25:08 +0800

    completed

--- src/Person.java
+++ src/Person.java
@@ -4,4 +4,37 @@ public class Person {
     private String lastName;
     private int age;
 
+    public Person(String firstName, String lastName, int age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName=firstName;
+    }
+
+    public void setlastName(String lastName){
+        this.lastName=lastName;
+    }
+
+    public void setAge(int age){
+        this.age=age;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public int getAge(){
+        return age;
+    }
+
+    public String toString(){
+        String output = "First name: " + firstName + ", Last name:" + lastName + ", Age: " + age;
+        return output;
+    }
+
 }
--- src/PersonFactory.java
+++ src/PersonFactory.java
@@ -9,14 +9,33 @@ public class PersonFactory {
     }
 
     public void addPerson(Person person) {
-
+        int num;
+        for(num = 0; num < allPersons.size()-1; num++){
+            if(person.getLastName().compareTo((allPersons.get(num)).getLastName()) < 0){
+                allPersons.add(num,person);
+                break;
+            }
+        }
     }
 
     public ArrayList<Person> under18() {
-
+        ArrayList<Person> under18 = new ArrayList<>();
+        for(int num=0;num<allPersons.size()-1;num++){
+            if(allPersons.get(num).getAge()<18){
+                under18.add(allPersons.get(num));
+            }
+        }
+        return under18;
     }
 
     public boolean checkLastName(String lastName) {
-
+        boolean checkLastName=false;
+        for(int num=0;num<allPersons.size()-1;num++){
+            if(lastName.compareTo(allPersons.get(num).getLastName())==0){
+                checkLastName = true;
+                break;
+            }
+        }
+        return checkLastName;
     }
 }
